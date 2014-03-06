1ee800d9ee76e9b8d93b0b42abc52b9ddc1d64ff
Geoff Unger <geoff@shopigniter.com>
2014-03-05 17:03:39 -0800

    Made a not-really-working WaveShaper

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ function init() {
     var g = au.context.createGain();
     g.gain.value = 0;
     var o = au.context.createOscillator();
+    var shaperArray = new Float32Array(1,1,1);
+    var w = au.context.createWaveShaper({'curve':'shaperArray'});
+
 
     $("#osc1-freq").change(function () {
 
@@ -33,15 +36,33 @@ function init() {
 
     $("input[name='osc1-radio']").click(function(){
         o.type = this.value;
-        console.log(this.value);
 
     });
+
+    $("#waveShaper1").change(function(){
+        shaperArray[0] = this.value;
+        w.curve = shaperArray;
+
+    });
+
+    $("#waveShaper2").change(function(){
+       shaperArray[1] = this.value;
+        w.curve = shaperArray;
+
+    });
+
+    $("#waveShaper3").change(function(){
+           shaperArray[2] = this.value;
+            w.curve = shaperArray;
+
+        });
 
 
 
     o.frequency.value = 440;
     o.type = "sine";
-    o.connect(g);
+    o.connect(w);
+    w.connect(g);
     g.connect(au.context.destination);
     o.start(0);
 
