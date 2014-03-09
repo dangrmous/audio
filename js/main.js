ba5f704b5443ec99f8353162fd216174d4d6a3ab
Geoffrey Unger <geoffunger@gmail.com>
2014-03-09 00:35:03 -0800

    Product pivot to Noiser

--- js/main.js
+++ js/main.js
@@ -13,8 +13,6 @@ var au = {};
 
     gain1.gain.value = 0;
 
-
-
     $("#osc1-freq").change(function () {
 
         var osc1Freq = this.value;
@@ -56,63 +54,13 @@ var au = {};
 
     });
 
-var threshold = -24; // dB
-var headroom = 21; // dB
-
-function e4(x, k)
-{
-    return 1.0 - Math.exp(-k * x);
-}
-
-function dBToLinear(db) {
-    return Math.pow(10.0, 0.05 * db);
-}
-
-function shape(x) {
-    var linearThreshold = dBToLinear(threshold);
-    var linearHeadroom = dBToLinear(headroom);
-
-    var maximum = 1.05 * linearHeadroom * linearThreshold;
-    var kk = (maximum - linearThreshold);
-
-    var sign = x < 0 ? -1 : +1;
-    var absx = Math.abs(x);
-
-    var shapedInput = absx < linearThreshold ? absx : linearThreshold + kk * e4(absx - linearThreshold, 1.0 / kk);
-    shapedInput *= sign;
-
-    return shapedInput;
-}
-
-function generateColortouchCurve(curve) {
-    var n = 44100;
-    var n2 = n / 2;
-
-    for (var i = 0; i < n2; ++i) {
-        x = i / n2;
-        x = shape(x);
-
-        curve[n2 + i] = x;
-        curve[n2 - i - 1] = -x;
+    curve = new Float32Array(44100);
+    for(i = 0; i < 44100; i++){
+        curve[i] = ((Math.random() * 2) -1);
     }
 
-    return curve;
-}
-
-function WaveShaper(context) {
-
-    var waveshaper = context.createWaveShaper();
-
-    var curve = new Float32Array(44100); // FIXME: share across instances
-    generateColortouchCurve(curve);
-    waveshaper.curve = curve;
-
-    return waveshaper;
-}
-
-    shaper1 = WaveShaper(au.context);
-
-
+    shaper1.curve = curve;
+    shaper1.oversample = "4x";
     osc1.frequency.value = 440;
     osc1.type = "sine";
     osc1.connect(shaper1);
