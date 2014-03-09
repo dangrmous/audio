d4b328332094e0c83368ffa3ee895be648dda8bf
Geoffrey Unger <geoffunger@gmail.com>
2014-03-09 13:36:04 -0700

    Delay now has feedback

--- js/main.js
+++ js/main.js
@@ -1,82 +1,97 @@
 var au = {};
 
 
-    // Fix up for prefixing
-    window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    au.context = new AudioContext();
+// Fix up for prefixing
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+au.context = new AudioContext();
 
-    var gain1 = au.context.createGain();
-    var filter1 = au.context.createBiquadFilter();
-    var shaper1 = au.context.createWaveShaper();
-    var osc1 = au.context.createOscillator();
-    var delay1 = au.context.createDelay();
-
-
-    gain1.gain.value = 0;
-
-    $("#osc1-freq").change(function () {
-
-        var osc1Freq = this.value;
-        var osc1FreqLog = Math.pow(osc1Freq, 2);
-        osc1.frequency.value = osc1FreqLog;
-        $("#osc1-freqValue").text(osc1FreqLog);
-    });
-
-    $("#osc1-vol").change(function () {
-
-        var volume = this.value;
-        var fraction = parseInt(this.value) / parseInt(this.max);
-        // Let's use an x*x curve (x-squared) since simple linear (x) does not
-        // sound as good.
-        gain1.gain.value = fraction * fraction;
-        $("#osc1-volValue").text(this.value);
+var gain1 = au.context.createGain();
+var gain2 = au.context.createGain();
+var filter1 = au.context.createBiquadFilter();
+var shaper1 = au.context.createWaveShaper();
+var osc1 = au.context.createOscillator();
+var delay1 = au.context.createDelay();
 
 
-    });
+gain1.gain.value = 0;
 
-    $("input[name='osc1-radio']").click(function () {
+$("#osc1-freq").change(function () {
 
-        osc1.type = this.value;
+    var osc1Freq = this.value;
+    var osc1FreqLog = Math.pow(osc1Freq, 2);
+    osc1.frequency.value = osc1FreqLog;
+    $("#osc1-freqValue").text(osc1FreqLog);
+});
 
-    });
-
-
-    $("#filter1").change(function () {
-
-        $("#filter1Value").text(this.value * this.value);
-        filter1.frequency.value = (this.value * this.value);
-
-    });
-
-    $("#filterQ1").change(function () {
-        filter1.Q.value = (this.value / 10000);
-        $("#filterQ1Value").text(this.value / 100000);
+$("#osc1-vol").change(function () {
 
 
-    });
+    var fraction = parseInt(this.value) / parseInt(this.max);
+    // Let's use an x*x curve (x-squared) since simple linear (x) does not
+    // sound as good.
+    gain1.gain.value = fraction * fraction;
+    $("#osc1-volValue").text(this.value);
 
-    curve = new Float32Array(44100);
-    for(i = 0; i < 44100; i++){
-        curve[i] = ((Math.random() * 2) -1);
-    }
 
-    $("#delay1").change(function(){
-        delay1.delayTime.value = this.value;
-        $("#delay1Value").text(this.value);
+});
 
-    })
+$("input[name='osc1-radio']").click(function () {
 
-    shaper1.curve = curve;
-    shaper1.oversample = "4x";
-    osc1.frequency.value = 440;
-    osc1.type = "sine";
-    osc1.connect(shaper1);
-    shaper1.connect(filter1);
-    filter1.connect(delay1);
-    filter1.connect(gain1);
-    delay1.connect(gain1);
-    gain1.connect(au.context.destination);
-    osc1.start(0);
+    osc1.type = this.value;
+
+});
+
+
+$("#filter1").change(function () {
+
+    $("#filter1Value").text(this.value * this.value);
+    filter1.frequency.value = (this.value * this.value);
+
+});
+
+$("#filterQ1").change(function () {
+    filter1.Q.value = (this.value / 10000);
+    $("#filterQ1Value").text(this.value / 100000);
+
+
+});
+
+curve = new Float32Array(44100);
+for (i = 0; i < 44100; i++) {
+    curve[i] = ((Math.random() * 2) - 1);
+}
+
+$("#delay1").change(function () {
+    delay1.delayTime.value = this.value;
+    $("#delay1Value").text(this.value);
+
+});
+
+$("#delay1fb").change(function () {
+
+
+    var fraction1 = parseInt(this.value) / parseInt(this.max);
+    // Let's use an x*x curve (x-squared) since simple linear (x) does not
+    // sound as good.
+    gain2.gain.value = fraction1 * fraction1;
+    $("#delay1fbValue").text(this.value);
+
+
+});
+
+shaper1.curve = curve;
+shaper1.oversample = "4x";
+osc1.frequency.value = 440;
+osc1.type = "sine";
+osc1.connect(shaper1);
+shaper1.connect(filter1);
+filter1.connect(delay1);
+filter1.connect(gain1);
+delay1.connect(gain1);
+delay1.connect(gain2);
+gain2.connect(delay1);
+gain1.connect(au.context.destination);
+osc1.start(0);
 
 
 
